f1134c84449f8f9dcc8f4f0ca3507a66d90124c6
Caterina Wang <caterinawang@Caterinas-MacBook-Pro.local>
2020-12-07 20:54:28 -0500

    Finalized files

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 # import necessary libraries
 import os
 from PIL import Image, ImageEnhance
-import cv2
+#import cv2
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
@@ -17,14 +17,16 @@ def resize_flip():
             im = Image.open(images_path + item)
             f, e = os.path.splitext(images_path + item)
             item_name = item[:-4]
-            imResize = im.resize((200, 200), Image.ANTIALIAS)
+            imResize = im.resize((256, 256), Image.ANTIALIAS)
             im_enhancer = ImageEnhance.Sharpness(imResize)
-            normal_sharpen = im_enhancer.enhance(2)
+            imResized = im_enhancer.enhance(2)
+            normal_sharpen = im_enhancer.enhance(4)
 
-            flipped_image = imResize.transpose(Image.FLIP_LEFT_RIGHT)
-            im_flip_enhancer = ImageEnhance.Sharpness(imResize)
-            flip_sharpen = im_flip_enhancer.enhance(2)
-            imResize.save("image_folder/Images/" + item_name + "_resized.jpg", 'JPEG', quality=90)
+            flipped = imResize.transpose(Image.FLIP_LEFT_RIGHT)
+            im_flip_enhancer = ImageEnhance.Sharpness(flipped)
+            flipped_image = im_flip_enhancer.enhance(2)
+            flip_sharpen = im_flip_enhancer.enhance(4)
+            imResized.save("image_folder/Images/" + item_name + "_resized.jpg", 'JPEG', quality=90)
             flipped_image.save("image_folder/Images/" + item_name + "_resized_flipped.jpg", 'JPEG', quality=90)
             normal_sharpen.save("image_folder/Images/" + item_name + "_resized_sharpen.jpg", 'JPEG', quality=90)
             flip_sharpen.save("image_folder/Images/" + item_name + "_resized_flipped_sharpen.jpg", 'JPEG', quality=90)
